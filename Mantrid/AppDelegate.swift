327cc02021ab9168e7b95ee91d0eb418a57e588a
puce <puce@macbook-pro.home>
2014-10-22 18:07:43 +0100

    Initial Commit

---
+++ Mantrid/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Mantrid
+//
+//  Created by puce on 22/10/2014.
+//  Copyright (c) 2014 puce. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
